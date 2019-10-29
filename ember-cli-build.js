148419746f6b68e6da527eef1d02901099c6c71a
Jen Weber <j@jenweber.me>
2019-10-29 18:34:28 -0400

    working codemirror

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,9 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
-
+  app.import('node_modules/codemirror/lib/codemirror.js')
+  app.import('node_modules/codemirror/mode/javascript/javascript.js')
+  app.import('node_modules/codemirror/lib/codemirror.css')
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
