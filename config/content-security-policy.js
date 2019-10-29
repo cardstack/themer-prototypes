6af54a20348f74a8861beaf9acf68e187b768c6c
Jen Weber <j@jenweber.me>
2019-10-29 12:47:27 -0400

    set CSP

---
+++ config/content-security-policy.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+'use strict';
+
+export default function(environment) {
+  return {
+    delivery: ['header'],
+    enabled: true,
+    failTests: true,
+    policy: {
+      'default-src':  ["'self'"],
+      'script-src':   ["'self'"],
+      'font-src':     ["'self'"],
+      'connect-src':  ["'self'"],
+      'img-src':      ["'self'"],
+      'style-src':    ["'self'"],
+      'media-src':    ["'self'"],
+    },
+    reportOnly: true,
+  };
+}
