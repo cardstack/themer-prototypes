ab2a3aa80e92fe8de691096662c4613a49275b9f
Jen Weber <j@jenweber.me>
2019-10-31 21:29:26 -0400

    wedding event example

--- app/services/code-samples.js
+++ app/services/code-samples.js
@@ -41,6 +41,73 @@ export default Service.extend({
   </div>
 </div>
   `,
-  eventCss: ``,
-  eventMarkup: ``
+  eventCss: `.default-card {
+  font-family: sans-serif;
+  text-align: center;
+}
+  
+.field-intro {}
+
+.field-spouse-one {
+  font-family: 'Sail', cursive;
+  font-size: 30px;
+}
+
+.field-spouse-two {
+  font-family: 'Sail', cursive;
+  font-size: 30px;
+}
+
+.field-wedding-date {}
+
+.field-location {}
+
+.field-rsvp-by {}
+
+.field-rsvp-link {}
+  `,
+  eventMarkup: `<div class="isolated-card default-card">
+  <div class="field-renderer view-field field-intro field-type-cardstack-core-types-string">
+    <span>
+      We are delighted to invite you to the wedding of
+    </span>
+  </div>
+  <div class="field-renderer view-field field-spouse-one field-type-cardstack-core-types-string">
+    <span>
+      Willa Karciana
+    </span>
+  </div>
+  and
+  <div class="field-renderer view-field field-spouse-two field-type-cardstack-core-types-string">
+    <span>
+      Ikram Eko
+    </span>
+  </div>
+  <div class="field-renderer view-field field-wedding-date field-type-cardstack-core-types-date">
+    <span>
+      August 8th, 2020
+    </span>
+  </div>
+
+  <div class="field-renderer view-field field-location field-type-cardstack-core-types-string">
+    <span>
+      The Gorgeous Hotel, 96 Hilton Avenue, San Francisco, CA
+    </span>
+  </div>
+  <div class="request-text">
+      Your RSVP is requested by
+  </div>
+
+  <div class="field-renderer view-field field-rsvp-by field-type-cardstack-core-types-date">
+    <span>
+      September 6th, 2020
+    </span>
+  </div>
+  <div class="field-renderer view-field field-rsvp-link field-type-cardstack-core-types-string">
+    <a href="#">
+      www.karciana-eko.example.com
+    </a>
+  </div>
+</div>
+`
 });
