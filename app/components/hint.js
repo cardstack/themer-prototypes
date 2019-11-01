b66ad276df06b431a3f4657aa40d1ec89fc235ab
Jen Weber <j@jenweber.me>
2019-11-01 15:32:15 -0400

    make editor readonly, reuse Hint element

--- app/components/hint.js
+++ app/components/hint.js
@@ -1,10 +1,13 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  showHint: false,
-  actions: {
-    toggleHint() {
-      this.toggleProperty('showHint')
-    }
+
+export default class HintComponent extends Component {
+  @tracked showHint = false;
+  
+  @action
+  toggleHint() {
+    this.showHint = !this.showHint
   }
-});
+}
