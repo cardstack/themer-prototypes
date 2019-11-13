323f410a0a181d6c8439811b5f2988bbcb827799
Jen Weber <j@jenweber.me>
2019-11-13 16:40:05 -0500

    documentation, and make updateCode action optional

--- app/components/code-editor.js
+++ app/components/code-editor.js
@@ -2,31 +2,28 @@ import Component from '@glimmer/component';
 import requirejsPolyfill from '@cardstack/requirejs-monaco-ember-polyfill';
 import * as monaco from 'monaco-editor';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
+// without this polyfill, workers won't start due to a type error accessing
+// a requirejs attribute. See the polyfill package readme for details.
 requirejsPolyfill();
 
-
 export default class CodeEditor extends Component {
-  @tracked code;
-  @tracked editor;
-  
-  constructor() {
-    super(...arguments);
-    this.code = this.args.code;
-  }
+  editor; // value is set by renderEditor
 
+  // readOnly defaults to true
   get readOnly() {
     if (this.args.readOnly === false) {
       return false;
+    } else {
+      return true;
     }
-
-    return true;
   }
 
   @action
   renderEditor(el) {
-    // monaco.editor.onDidCreateEditor(this.onEditorReady)
+    // This is called when the containing div has been rendered.
+    // `create` constructs a code editor and inserts it into the DOM.
+    // el is the element that {{did-insert}} was used on.
     let editor = monaco.editor.create(el, {
       value: this.args.code,
       language: this.args.language,
@@ -34,18 +31,24 @@ export default class CodeEditor extends Component {
       readOnly: this.readOnly
     });
     
+    // Whenever the code block's text changes, onUpdateCode will be called.
     editor.onDidChangeModelContent(this.onUpdateCode)
+
+    // Save editor instance locally, so we can reference it in other methods
     this.editor = editor;
   }
 
   @action
   onEditorReady() {
-    console.log('EDITOR READY');
-    // https://microsoft.github.io/monaco-editor/api/modules/monaco.editor.html#ondidcreateeditor
+    // console.log('editor ready')
   }
 
   @action
   onUpdateCode() {
-    this.args.updateCode(this.editor.getValue())
+    if (this.args.updateCode) {
+      // passing the updateCode action in is optional, so we check to see if it's
+      // defined before calling it.
+      this.args.updateCode(this.editor.getValue())
+    }
   }
 }
